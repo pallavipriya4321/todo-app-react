96c1ffe8344f41c9ed8dc3b8e525cd73ccbe83dd
Pallavi Priya <pallavipriya4321@gmail.com>
2025-04-18 03:46:22 +0530

    add feature to toggle todo status

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ const API_URL = "http://localhost:8080/api/v1";
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(false);
-  const fetchTodos = async () => {
-    setLoading(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const fetchTodos = async (refresh: boolean = true) => {
+    refresh ? setRefreshing(true) : setLoading(true);
     const todosResponse = await fetch(API_URL + "/todos");
     if (todosResponse.status === 200) {
       const todosJson = await todosResponse.json();
-
+      (todosJson.data as Todo[]).sort((t1, t2) => {
+        return (
+          new Date(t1.createdAt).getTime() - new Date(t2.createdAt).getTime()
+        );
+      });
       setTodos(todosJson.data as Todo[]);
     }
-    setLoading(false);
+    refresh ? setRefreshing(false) : setLoading(false);
   };
   useEffect(() => {
     fetchTodos();
@@ -26,7 +31,25 @@ function App() {
           ? "Loading..."
           : todos.length > 0
           ? todos.map((todo) => {
-              return <li key={todo._id}>{todo.title}</li>;
+              return (
+                <li key={todo._id}>
+                  {" "}
+                  <input
+                    type="checkbox"
+                    onChange={async () => {
+                      await fetch(
+                        API_URL + `/todos/toggle/status/${todo._id}`,
+                        {
+                          method: "PATCH",
+                        }
+                      );
+                      fetchTodos();
+                    }}
+                    checked={todo.isComplete}
+                  />{" "}
+                  {todo.title}
+                </li>
+              );
             })
           : "No Todos"}
       </div>
